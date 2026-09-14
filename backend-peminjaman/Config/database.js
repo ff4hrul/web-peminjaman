d5e90f80a0580645b742eda2c5a3f57a6d114968
Mochamad Fahrul Rizqi <rullll1803@gmail.comm>
2026-09-15 01:11:15 +0700

    Fix mysql2 loading on Vercel

--- backend-peminjaman/Config/database.js
+++ backend-peminjaman/Config/database.js
@@ -1,11 +1,18 @@
 const { Sequelize } = require('sequelize');
+const mysql2 = require('mysql2');
+
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'mysql',
+
+  // Paksa Vercel menggunakan mysql2
+  dialectModule: mysql2,
+
   logging: false,
+
   dialectOptions: {
     ssl: {
       require: true,
