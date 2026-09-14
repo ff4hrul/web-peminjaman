c9bffeb33de1837785a4a18bf3c5c83ce451d5e1
Mochamad Fahrul Rizqi <rullll1803@gmail.comm>
2026-09-14 23:36:21 +0700

    Update production API URL

--- backend-peminjaman/Config/database.js
+++ backend-peminjaman/Config/database.js
@@ -1,9 +1,17 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite', // File database otomatis dibuat di sini
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
